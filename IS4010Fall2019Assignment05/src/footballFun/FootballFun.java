153c51596f04da7cc7edb1ca341cb9db63fdb451
Bill Nicholson <nicholdw@ucmail.uc.edu>
2019-09-26 07:36:16 -0400

    Uploaded incomplete project with empty method

---
+++ IS4010Fall2019Assignment05/src/footballFun/FootballFun.java
@@ -0,0 +1,27 @@
+/*
+ * IS4010 Fal 2019
+ * Assignment 05
+ * Bill Nicholson
+ * nicholdw@ucmail.uc.edu
+ */
+package footballFun;
+
+public class FootballFun {
+
+	public static void main(String[] args) {
+		int correctCount = 0;
+		if (translateScore(2).equals("safety") == true) { correctCount++;}
+		if (translateScore(3).equals("field goal") == true) { correctCount++;}
+		if (translateScore(6).equals("touchdown") == true) { correctCount++;}
+		if (translateScore(7).equals("Touchdown and extra point") == true) { correctCount++;}
+		if (translateScore(8).equals("Touchdown and 2-point conversion") == true) { correctCount++;}
+		if (correctCount == 5) {
+			System.out.println("All tests passed");
+		} else {
+			System.out.println("At least one test failed");
+		}
+	}
+	public static String translateScore (int score) {
+		return "";
+	}
+}
